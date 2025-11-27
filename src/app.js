91054f1268a6597bbf6eefeab344005ae4fe37ef
cxz <1054757289@qq.com>
2025-11-27 22:44:29 +0800

    fix: 限制CORS来源为PROJECT_URL环境变量

--- src/app.js
+++ src/app.js
@@ -10,10 +10,16 @@ import urlRedirectRouter from "./routes/urlRedirectRoute.js"
 import rateLimiter from "./utils/rateLimiter.js"
 import { pinoHttpMiddleware } from "./utils/loggerHelper.js"
 
+const PROJECT_URL = process.env.PROJECT_URL
+
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+  {
+    origin: PROJECT_URL,
+  }
+))
 app.use("/v1/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(rateLimiter)
